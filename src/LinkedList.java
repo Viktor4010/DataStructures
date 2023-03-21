4be143a6b93613b750a3f83e6a8f8af5aecdb3b7
Viktor4010 <avacode78@gmail.com>
2023-03-21 13:57:05 +0700

    implement print(), addFirst(), addLast(), methods. Extract logic where checking weather LinkedList is empty or not into separate private method

---
+++ src/LinkedList.java
@@ -0,0 +1,57 @@
+public class LinkedList {
+
+    private class Node {
+
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void print() {
+        Node current = first;
+
+        while (current != null) {
+            System.out.println(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+
+        size++;
+    }
+
+    public void addLast(int item) {
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+
+}
