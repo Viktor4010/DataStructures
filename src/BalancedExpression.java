fcc87b37387366b20ab19f895563dfbf9ed1dcd5
Viktor4010 <avacode78@gmail.com>
2023-03-21 17:17:17 +0700

    implement decision of the task called BalancedExpression

---
+++ src/BalancedExpression.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.Stack;
+
+public class BalancedExpression {
+    private final List<Character> leftBrackets = List.of('(', '<', '[', '{');
+    private final List<Character> rightBrackets = List.of(')', '>', ']', '}');
+
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            if (isLeftBracket(ch))
+                stack.push(ch);
+
+            if (isRightBracket(ch)) {
+                if (stack.empty()) return false;
+
+                Character top = stack.pop();
+                if (!bracketsMatch(top, ch)) return false;
+            }
+        }
+
+        return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
+    }
+}
