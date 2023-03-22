20e2391b126d8543b9f8087477a49f771bf766ed
Viktor4010 <avacode78@gmail.com>
2023-03-22 11:34:14 +0700

    Solve two tasks using HashMap and HashSet

---
+++ src/CharFinder.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CharFinder {
+
+    public char findFirstNonRepeatingChar(String str) {
+        // key - char, value - amount of char appearance in String
+        Map<Character, Integer> map = new HashMap<>();
+
+
+        for (char c : str.toCharArray()) {
+            Integer count = map.getOrDefault(c, 0);
+            map.put(c, count + 1);
+        }
+
+        for (char ch : str.toCharArray()) {
+            if (map.get(ch) == 1)
+                return ch;
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    public char findFirstRepeatedChar(String str) {
+        Set<Character> set = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            if (set.contains(ch))
+                return ch;
+
+            set.add(ch);
+        }
+
+        return Character.MIN_VALUE;
+    }
+}
